d3f75ea0b09acf7cc73df461bb8cbfa500cd59e0
thd7830 <thd7830@outlook.kr>
2021-08-19 17:45:13 +0900

    20210819 업로드

--- NewStructPrintScore/NewStructPrintScore.cpp
+++ NewStructPrintScore/NewStructPrintScore.cpp
@@ -33,6 +33,7 @@ typedef struct {
 	double avg;
 } STU;
 
+
 STU student[nArr];
 void totalswap(void* a, void* b, int op)
 {
@@ -64,22 +65,22 @@ void totalswap(void* a, void* b, int op)
 	}
 }
 
-//void sort(double* a, int op)
-//{
-//	int i, j, k;
-//	for (i = 0; i < nArr; i++)
-//	{
-//		for (j = 0; j < nArr; j++)
-//		{
-//			if (a[i] < a[j])
-//			{
-//				totalswap(a + i, a + j, 8);
-//				totalswap(student + i, student + j, 18);
-//				// 문자열의 배열 == *
-//			}
-//		}
-//	}
-//}
+void sort(double* a, int op)
+{
+	int i, j, k;
+	for (i = 0; i < nArr; i++)
+	{
+		for (j = 0; j < nArr; j++)
+		{
+			if (a[i] > a[j])
+			{
+				totalswap(a + i, a + j, sizeof(a));
+				totalswap(student + i, student + j, sizeof(student));
+				// 문자열의 배열 == *
+			}
+		}
+	}
+}
 // 학생 수는 10명이니, 구조체 배열을 선언해보자.
 
 void sortSTU(STU* a, int op)
@@ -96,124 +97,179 @@ void sortSTU(STU* a, int op)
 		}
 	}
 }
-//int sortTest()
-//{
-//	double f_kor = 0.3, f_eng = 0.7;	// 가중치
-//	double tot[nArr];
-//	int i, j, k;
-//
-//
-//	for (int i = 0; i < nArr; i++)
-//	{
-//		//student[i].kor = kor[i];
-//		//student[i].eng = eng[i];
-//		strcpy(student[i].name, name[i]);
-//		tot[i] = (student[i].kor = kor[i]) * f_kor + (student[i].eng = eng[i]) * f_eng;
-//		// tot[i] = student.kor[i] * f_kor + student.eng[i] * f_eng;
-//	}
-//
-//
-//	printf("Original : \n");
-//	printf("이름 : "); for (int i = 0; i < nArr; i++) printf("%7s ", student[i].name); printf("\n\n");
-//	printf("국어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].kor); printf("\n\n");
-//	printf("영어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].eng); printf("\n\n");
-//	printf("합계 : "); for (int i = 0; i < nArr; i++) printf("%7.2f ", tot[i]); printf("\n\n");
-//
-//	sort(tot, nArr);		// 정렬
-//
-//	printf("Sorted : \n");
-//	printf("이름 : "); for (int i = 0; i < nArr; i++) printf("%7s ", student[i].name); printf("\n\n");
-//	printf("국어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].kor); printf("\n\n");
-//	printf("영어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].eng); printf("\n\n");
-//	printf("합계 : "); for (int i = 0; i < nArr; i++) printf("%7.2f ", tot[i]); printf("\n\n");
-//
-//	return 0;
-//}
+int sortTest()
+{
+	double f_kor = 0.3, f_eng = 0.7;	// 가중치
+	double tot[nArr];
+	//double *tot  = (double *)malloc(sizeof(double)*nArr);
+
+
+	for (int i = 0; i < nArr; i++)
+	{
+		//student[i].kor = kor[i];
+		//student[i].eng = eng[i];
+		strcpy(student[i].name, name[i]);  // 문자열(널 포함 0)은 대입 연산자를 사용할 수 없다. 그래서 strcpy 사용
+			// strcpy는 크기에 제한이 없다. 하지만 오류가 발생할 수 있으므로, strncpy 함수를 사용하기도 한다.
+			// strncpy의 n : 복사할 문자 수
+			// size_t는? 변수의 type으로, long이라고 생각하면 된다.
+ 
+			// strcpy는 '\0'까지 복사된다.
+			// strncpy는 '\0'이 복사가 되지 않는다.
+ 
+ 
+        // 예시
+		//		char *a = "Good morning";
+		//		char buf[10];
+		//		
+		//		strncpy(buf, a, 4);  // null 없음
+		//		buf[4] = 0;  // null (반드시 필요한 과정)
+		//		printf("%s", buf);  // Good??
+ 
+		tot[i] = (student[i].kor = kor[i]) + (student[i].eng = eng[i]);
+		student[i].avg = (student[i].kor = kor[i]) * f_kor + (student[i].eng = eng[i]) * f_eng;
+	}
+
+
+	printf("Original : \n");
+	printf("이름 : "); for (int i = 0; i < nArr; i++) printf("%7s ", student[i].name); printf("\n\n");
+	printf("국어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].kor); printf("\n\n");
+	printf("영어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].eng); printf("\n\n");
+	printf("합계 : "); for (int i = 0; i < nArr; i++) printf("%7.2f ", tot[i]); printf("\n\n");
+	printf("평균 : "); for (int i = 0; i < nArr; i++) printf("%7.2f ", student[i].avg); printf("\n\n");
+
+	sortSTU(student, nArr);		// 정렬
+
+	printf("Sorted : \n");
+	printf("이름 : "); for (int i = 0; i < nArr; i++) printf("%7s ", student[i].name); printf("\n\n");
+	printf("국어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].kor); printf("\n\n");
+	printf("영어 : "); for (int i = 0; i < nArr; i++) printf("%7d ", student[i].eng); printf("\n\n");
+	printf("합계 : "); for (int i = 0; i < nArr; i++) printf("%7.2f ", tot[i]); printf("\n\n");
+	printf("평균 : "); for (int i = 0; i < nArr; i++) printf("%7.2f ", student[i].avg); printf("\n\n");
+
+	return 0;
+}
 
 
 int sortTestEx() // 구조체를 이용한 성적 처리 - 파일 입출력
 {
 	int n = 10;
 	double f_kor = 0.3, f_eng = 0.7;	// 가중치
-	double tot[nArr], avg[nArr];
+	//double tot[nArr], avg[nArr];
 	int i, j, k;
+
+	//****************************************************************************
+	//****************************************************************************
+	//****************************************************************************
+	int num;   // file에서 read
+	STU *students;  // malloc 으로 메모리 확보
+	//****************************************************************************
+	//****************************************************************************
+	//****************************************************************************
+
+
 
 	FILE* fin = fopen("C:\\Users\\KOSTA\\table1.txt", "r");
 	FILE* fout = fopen("C:\\Users\\KOSTA\\table1.rpt", "w+b");
-	for (i = 0; i < nArr; i++)	fscanf(fin, "%s", student[i].name); // Debugging
-	for (i = 0; i < nArr; i++)	fscanf(fin, "%d", &student[i].kor);
-	for (i = 0; i < nArr; i++)	fscanf(fin, "%d", &student[i].eng);
+	fscanf(fin, "%d", &num);
+	students = (STU *)malloc(sizeof(STU)*num);
+	for (i = 0; i < nArr; i++)	fscanf(fin, "%s", students[i].name); // Debugging
+	for (i = 0; i < nArr; i++)	fscanf(fin, "%d", &students[i].kor);
+	for (i = 0; i < nArr; i++)	fscanf(fin, "%d", &students[i].eng);
 	for (i = 0; i < nArr; i++)
 	{
-		student[i].tot = student[i].kor + student[i].eng;
-		student[i].avg = student[i].tot/2;
+		students[i].tot = students[i].kor + students[i].eng;
+		students[i].avg = students[i].tot/2;
 	}
 
 	fprintf(fout, "Original : \n"); fprintf(fout,"%-7s %-7s %-7s %-7s %-7s\n", "  이름", "  국어", "  영어", "  총점", "  평균");
 	for (int i = 0; i < n; i++)
 	{
-		fprintf(fout,"%7s %7d %7d %7.2f %7.2f\n", student[i].name,
-		student[i].kor, student[i].eng, student[i].tot, student[i].avg);
+		fprintf(fout,"%7s %7d %7d %7.2f %7.2f\n", students[i].name,
+		students[i].kor, students[i].eng, students[i].tot, students[i].avg);
 	}
 
-	sortSTU(student, nArr);		// 정렬
+	sortSTU(students, nArr);		// 정렬
 
 	fprintf(fout,"\n\nSorted : \n");
 	for (int i = 0; i < n; i++)
 	{
-		fprintf(fout,"%7s %7d %7d %7.2f %7.2f\n", student[i].name,
-			student[i].kor, student[i].eng, student[i].tot, student[i].avg);
+		fprintf(fout,"%7s %7d %7d %7.2f %7.2f\n", (students+i)->name,
+			(students+i)->kor, (students+i)->eng, (students+i)->tot, (students+i)->avg);
 	}
+
+	fclose(fin);   // 파일을 열었다면 닫아야 한다는 생각을 가질 수 있도록 하자.
+	fclose(fout);
+	// 한 방에 닫기 위해서는?
+	// fcloseall();  // 현재 열려 있는 모든 파일을 닫으라는 뜻
 
 	return 0;
 }
 
 
-/*void StreamTest()
+void StreamTest()
 {
 	int m, i, j;
 	float d;
 	char buf[1024];
 
 	// cmd -> echo "입력할 내용" > "저장 파일명.저장 파일확장자"
-	FILE* fin = fopen("C:\\Users\\KOSTA\\table1.txt", "r");  
-	FILE* fout = fopen("C:\\Users\\KOSTA\\tabler.txt", "a+b");
+	FILE* fin = fopen("C:\\Users\\KOSTA\\aa", "r");  
+	FILE* fout = fopen("C:\\Users\\KOSTA\\aa.re", "a+b");
 					// r : read, w : write(overwrite), a : append(추가)
 					// 만약 파일이 존재하지 않으면 -1 반환. 즉 오류 반환
 	if (fin != NULL)
 	{
 		// CRLF의 white space 제거해야 함.
-		fscanf(fin, "%s %d %d", buf[j], &m[j], &i[j]); // 데이터 입력 -> 구조체로 load -> swap, sorting -> 출력
-		fprintf(fout, "이름 : %s \n 국어 :%d\n 영어: %d\n", buf, m, i); // 출력
-		printf("이름 : %s \n 국어 :%d\n 영어: %d\n", buf, m, i);
-		//while (1)
-		//{
-		//	//fgets(buf, 1024, stdin);				// 파일 입력
-		//	//										// stdin : 이미 존재하고 있는 파일
-		//	//if (strlen(buf) < 3) break;				// enter 자체도 \n이므로 문자열 길이가 0이 아니다.
-		//	//fputs("==== 입력문자열 ====>", stdout);		// stdout : 이미 존재하고 있는 파일 -> 언제든 사용이 가능하다는 것
-		//	
-		//	if (fgets(buf, 1024, fin) == NULL) break;
-		//	fputs(buf, stdout);
-		//	//fputs(buf, fout0);
-		//	//fputs(buf, fout1);
-		//	//fputs(buf, fout2);
-		//	// 차이점
-		//	// aa.o1은 메모장을 실행하면 Windows(CRLF) 형식으로 나온다.
-		//	// aa.02는 메모장을 실행하면 Linux(LF) 형식으로 나온다. binary 모드로 출력하였을 때,
-		//	// 즉, CR이 사라짐
+		fscanf(fin, "%d %f %s", &m, &d, buf); // 데이터 입력 -> 구조체로 load -> swap, sorting -> 출력
+		fprintf(fout, "정수 : %d \n 실수 :%f\n 인사: %s\n", m, d, buf); // 출력
+		printf("정수 : % d \n 실수 : % f\n 인사 : % s\n", m, d, buf);
+		while (1)
+		{
+			//fgets(buf, 1024, stdin);				// 파일 입력
+			//										// stdin : 이미 존재하고 있는 파일
+			//if (strlen(buf) < 3) break;				// enter 자체도 \n이므로 문자열 길이가 0이 아니다.
+			//fputs("==== 입력문자열 ====>", stdout);		// stdout : 이미 존재하고 있는 파일 -> 언제든 사용이 가능하다는 것
+			
+			if (fgets(buf, 1024, fin) == NULL) break;
+			fputs(buf, stdout);
+			//fputs(buf, fout0);
+			//fputs(buf, fout1);
+			//fputs(buf, fout2);
+			// 차이점
+			// aa.o1은 메모장을 실행하면 Windows(CRLF) 형식으로 나온다.
+			// aa.02는 메모장을 실행하면 Linux(LF) 형식으로 나온다. binary 모드로 출력하였을 때,
+			// 즉, CR이 사라짐
 
-		//	// fscanf( , , , , )
-		//	// fprintf( , , , , )
-		//}
+			// fscanf( , , , , )
+			// fprintf( , , , , )
+		}
 		
 	}
 	else printf("입력 파일이 존재하지 않습니다.\n");
-}*/
+}
 
 int main()
 {
-	//sortTest();
-	//StreamTest();
-	sortTestEx();
+	int k;
+	printf(
+		"	1.sortTest();\n"
+		"	2.StreamTest();\n"
+		"	3.sortTestEx();\n"
+		"	0. Exit\n\n"
+		"======================"
+		"	Select Menu : ");
+
+	scanf("%d", &k);
+
+	switch (k)
+	{
+		// sortTest();    // 1
+		// StreamTest();  // 2
+		// sortTestEx();  // 3
+	case 1:	sortTest();		break;
+	case 2: StreamTest();	break;
+	case 3:	sortTestEx();	break;
+	case 4: case 5: case 6: case 7: case 8: case 9:
+	default:	break;
+	}
 }
