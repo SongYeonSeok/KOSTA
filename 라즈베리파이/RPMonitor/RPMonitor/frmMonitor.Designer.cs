8cf5b6e788ec855795ca22da6372651b2cc8d654
Yeonseok <49339278+SongYeonSeok@users.noreply.github.com>
2021-11-08 18:12:43 +0900

    Add files via upload

---
+++ 라즈베리파이/RPMonitor/RPMonitor/frmMonitor.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace RPMonitor
+{
+    partial class frmMonitor
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tbPortNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbMemo = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(3, 3);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 23;
+            this.dataGrid.Size = new System.Drawing.Size(842, 208);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 466);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(845, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(845, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tbPortNum
+            // 
+            this.tbPortNum.Location = new System.Drawing.Point(53, 17);
+            this.tbPortNum.Name = "tbPortNum";
+            this.tbPortNum.Size = new System.Drawing.Size(113, 21);
+            this.tbPortNum.TabIndex = 3;
+            this.tbPortNum.Text = "9000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Port";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(172, 15);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbMemo
+            // 
+            this.tbMemo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMemo.Location = new System.Drawing.Point(3, 3);
+            this.tbMemo.Multiline = true;
+            this.tbMemo.Name = "tbMemo";
+            this.tbMemo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbMemo.Size = new System.Drawing.Size(505, 209);
+            this.tbMemo.TabIndex = 6;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(845, 436);
+            this.splitContainer1.SplitterDistance = 218;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer2.Location = new System.Drawing.Point(0, 3);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tbMemo);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Panel2.Controls.Add(this.btnStart);
+            this.splitContainer2.Panel2.Controls.Add(this.tbPortNum);
+            this.splitContainer2.Size = new System.Drawing.Size(845, 212);
+            this.splitContainer2.SplitterDistance = 508;
+            this.splitContainer2.TabIndex = 7;
+            // 
+            // frmMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 488);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMonitor";
+            this.Text = "RaspberryPi Monitor ver. 1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMonitor_FormClosing);
+            this.Load += new System.EventHandler(this.frmMonitor_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMonitor_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox tbPortNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbMemo;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+    }
+}
+
---
+++ 라즈베리파이/RPMonitor/RPMonitor/frmMonitor.cs
@@ -0,0 +1,172 @@
+﻿using myLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPMonitor
+{
+    public partial class frmMonitor : Form
+    {
+        public frmMonitor()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 폼을 시작할 때...
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMonitor_Load(object sender, EventArgs e)
+        {
+            sqldb = new SqlDB(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\송연석\source\repos\RPDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+
+        SqlDB sqldb;
+        TcpClient tcp = null;
+        TcpListener listen = null;
+        Socket sock = null;
+        Thread serverThread = null;
+        Thread readThread = null;
+        Thread clientReadThread = null;
+        string connectIP = "192.168.2.60";
+        int port = 9000;
+
+        delegate void CB(string s);
+        /// <summary>
+        /// tbMemo에 텍스트 추가
+        /// </summary>
+        /// <param name="str"></param>
+        void AddText(string str)
+        {
+            // tbMemo에 대해서 InvokeRequired값이 True이면, CB를 사용한다.
+            if (tbMemo.InvokeRequired)
+            {
+                CB cb = new CB(AddText);
+                object[] obj = { str };
+                Invoke(cb, obj);
+            }
+            else
+            {
+                // invoke가 required되지 않으면
+                tbMemo.Text += str;
+            }
+        }
+
+
+        /// <summary>
+        /// tbPortNum에 값을 입력하였다면, port 변수에 값을 대입한다.
+        /// 그리고 listen에 port값을 입력하고 listen을 시작한다. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (tbPortNum.Text != "") port = int.Parse(tbPortNum.Text);
+            listen = new TcpListener(port);
+            listen.Start();
+            serverThread = new Thread(ServerProcess);
+            serverThread.Start();
+            readThread = new Thread(ReadProcess);
+            readThread.Start();
+
+            AddText("Server Started");
+            btnStart.Enabled = false;       // 연결 후에 버튼을 비활성화시킴
+        }
+        
+        /// <summary>
+        /// 외부로부터 클라이언트 요청이 있을 때까지 돌아간다.
+        /// Thread가 중지될 때까지 돌아간다.
+        /// </summary>
+        void ServerProcess()
+        {
+            while (true)
+            {
+                if (listen.Pending())   // 외부로부터 클라이언트 요청이 있는가? (O -> True, X -> False)
+                {
+                    tcp = listen.AcceptTcpClient();     // tcp는 클라이언트의 요청을 수락한다.
+                    tcp.Client.Send(Encoding.Default.GetBytes("Hello"));    // 그 후에 Server가 client로 hello라는 글을 전송
+                }
+                Thread.Sleep(1000);     // 쉬어가기
+            }
+        }
+
+
+
+        /// <summary>
+        /// 데이터를 가져오는 과정
+        /// </summary>
+        void ReadProcess()
+        {
+            byte[] bArr = new byte[1024];
+            int Count = 0;
+            Count = int.Parse(sqldb.GetString("select max(id) from RPLog")) + 1;
+            while(true)
+            {
+                if(tcp != null)
+                {
+                    NetworkStream ns = tcp.GetStream();
+                    if(ns.DataAvailable)        // 읽어올 데이터가 있다면?
+                    {
+                        try
+                        {
+                            int n = ns.Read(bArr, 0, 1024);      // 읽어오기
+                            if (n == 1 && bArr[0] == 0x1b)       // ESC이면(27 == 0x1b)
+                            {
+                                tcp.Close(); tcp = null; continue;   // 계속
+                            }
+                            if (bArr[n - 1] == '\r') tcp.Client.Send(Encoding.Default.GetBytes("ACK"));     // ACK
+                            byte[] bArr2 = Encoding.Convert(Encoding.UTF8, Encoding.Default, bArr, 0, n);
+                            string str = Encoding.Default.GetString(bArr2);
+                            string[] sa = str.Split(',');
+                            string sql = $"insert into RPLog values ({Count++}, '00001', {sa[0]}, {sa[1]}, {sa[2]}, {sa[3]}, {sa[4]}, {sa[5]}, {sa[6]}, {DateTime.Now})";
+                            sqldb.Run(sql);     // sql 구문 수행
+                            AddText(str + "\r\n");
+                        }
+                        catch(Exception e1)
+                        {
+                            MessageBox.Show(e1.Message);
+                        }
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void frmMonitor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseProcess();
+        }
+
+        /// <summary>
+        /// 스레드 종료 함수
+        /// </summary>
+        void CloseProcess()
+        {
+            if (serverThread != null) serverThread.Abort();
+            if (readThread != null) readThread.Abort();
+        }
+        /// <summary>
+        /// ESC 키를 눌렀을 경우, Close 세션 종료
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMonitor_KeyDown(object sender, KeyEventArgs e)      // key preview => true로 변경
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+
+    }
+}
